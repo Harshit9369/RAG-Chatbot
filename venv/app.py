cf153fcb60ddf40fd98aa43f09f69b2343f5a688
Harshit9369 <harshitagrwl911@gmail.com>
2024-10-22 01:46:13 +0530

    embeddings code added

--- venv/app.py
+++ venv/app.py
@@ -60,3 +60,97 @@ async def ask_question(input_data: UserInput):
 async def root():
     return {"message": "Medical Chatbot API is live!"}
 
+# """
+# import fitz  #
+# from sentence_transformers import SentenceTransformer
+# from pinecone import Pinecone, ServerlessSpec
+# import time
+# from math import ceil
+
+# def extract_text_from_pdf(pdf_path):
+#     doc = fitz.open(pdf_path)
+#     text = ""
+#     for page in doc:
+#         text += page.get_text("text")
+#     return text
+
+# pdf_paths = [
+#     "../Docs/ASCO- Non-Small Cell Lung Cancer.pdf",
+#     "../Docs/ASCO- Small Cell Lung Cancer.pdf",
+#     "../Docs/ESMO- Early and Locally Advanced Non Small Cell Lung Cancer HP Guide.pdf",
+#     "../Docs/ESMO- Malignant Pleural Mesothelioma HP Guide.pdf",
+#     "../Docs/ESMO- Non Oncogene Addicted Metastatic NSCLC HP Guide.pdf",
+#     "../Docs/ESMO- Non-small-cell lung cancer (NSCLC)_ An ESMO guide for patients.pdf",
+#     "../Docs/ESMO- Oncogene Addicted Metastatic NSCLC HP Guide (copy).pdf",
+#     "../Docs/ESMO- Small Cell Lung Cancer HP Guide.pdf",
+#     "../Docs/ESMO- Thymic Epithelial Tumours HP Guide.pdf",
+#     "../Docs/NCCN- Metastatic Non Small Cell Lung Cancer Patient Guide.pdf",
+#     "../Docs/NCCN- Non Small Cell Lung Cancer HP Guide .pdf",
+#     "../Docs/NCCN- Non Small Cell Lung Cancer Patient Guide.pdf",
+#     "../Docs/NCCN- Small Cell Lung Cancer HP Guide.pdf",
+#     "../Docs/NCCN- Small Cell Lung Cancer Patient Guide.pdf"
+# ]
+
+# pdf_texts = []
+
+# for pdf_path in pdf_paths:
+#     pdf_texts.append(extract_text_from_pdf(pdf_path))
+
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# pinecone_api_key = "70eaf2cd-563b-46e0-af9a-ba4667bc0198"
+# pinecone_environment = "us-east-1"
+
+# index_name = "rag-chatbot-medical"
+
+# pc = Pinecone(api_key="70eaf2cd-563b-46e0-af9a-ba4667bc0198")
+
+# pc.create_index(    
+#     name=index_name,
+#     dimension=384,
+#     metric="cosine",
+#     spec=ServerlessSpec(
+#         cloud="aws",
+#         region="us-east-1"
+#     )
+# )
+
+# def chunk_text(text, max_bytes):
+#     chunks = []
+#     current_chunk = []
+#     current_size = 0
+#     for sentence in text.split('. '):
+#         sentence += '. '
+#         sentence_size = len(sentence.encode('utf-8'))
+#         if current_size + sentence_size <= max_bytes:
+#             current_chunk.append(sentence)
+#             current_size += sentence_size
+#         else:
+#             chunks.append(''.join(current_chunk))
+#             current_chunk = [sentence]
+#             current_size = sentence_size
+#     if current_chunk:
+#         chunks.append(''.join(current_chunk))
+#     return chunks
+
+# max_metadata_size = 40950
+
+# while not pc.describe_index(index_name).status['ready']:
+#     time.sleep(1)
+
+# index = pc.Index(index_name)
+
+# for pdf_id, pdf_text in enumerate(pdf_texts):
+#     chunks = chunk_text(pdf_text, max_metadata_size)
+#     chunk_embeddings = model.encode(chunks)
+
+#     vectors = []
+#     for i, (chunk, embedding) in enumerate(zip(chunks, chunk_embeddings)):
+#         vectors.append({
+#             "id": f"pdf-{pdf_id}-chunk-{i}",
+#             "values": embedding.tolist(),
+#             "metadata": {'text': chunk}
+#         })
+    
+#     index.upsert(vectors=vectors, namespace=namespace)
+# """
